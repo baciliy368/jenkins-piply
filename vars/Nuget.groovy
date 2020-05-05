314202615be9b07e56a0c0e9375c2bd5d97784cb
d2.orlov <orlovfinder@gmail.com>
2020-05-05 13:09:51 +0300

    фыв

--- vars/Nuget.groovy
+++ vars/Nuget.groovy
@@ -1,5 +1,5 @@
-def call(String pathToNuget, String command) {
-    dir("\\PaintTests") {
+def call(String projectFolderName, String pathToNuget, String command) {
+    dir(String.format("\\%s", projectFolderName)) {
         bat String.format('%s %s',pathToNuget, command)
     }
 }

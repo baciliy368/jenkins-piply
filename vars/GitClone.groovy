0d2b554377b2f1f0b34ba259094dca4b2ba4ff81
d2.orlov <orlovfinder@gmail.com>
2020-05-05 11:32:53 +0300

    фыв

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -1,3 +1,4 @@
 def call(String credentials, String  branch, String url) {
-    git branch: branch, changelog: false, credentialsId: credentials, poll: false, url: url
+    git branch: 'paint_sharp', credentialsId: '6a6c48eb-48f3-4481-a049-e6c170f076aa', url: 'http://qa-gitlab.itransition.corp/self-education/d2.orlov'
+    //git branch: branch, changelog: false, credentialsId: credentials, poll: false, url: url
 }

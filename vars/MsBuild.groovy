d2f9579ce5bff182c672e6e5f6ab4952fc7ff87e
d2.orlov <orlovfinder@gmail.com>
2020-05-05 14:43:01 +0300

    фыв

--- vars/MsBuild.groovy
+++ vars/MsBuild.groovy
@@ -1,5 +1,7 @@
 def call(String projectFolderName, String pathToMsBuild) {
-    bat String.format('%s %s',pathToMsBuild, getPathToSolution(projectFolderName))
+    dir(${workspace}) {
+        bat String.format('%s %s',pathToMsBuild, getPathToSolution(projectFolderName))
+    }
 }
 
 def getPathToSolution(String projectName) {

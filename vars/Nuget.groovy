97fddca3bf90921be666ea8ec1d3b39529e581c6
d2.orlov <orlovfinder@gmail.com>
2020-05-05 13:00:18 +0300

    фыв

--- vars/Nuget.groovy
+++ vars/Nuget.groovy
@@ -1,7 +1,3 @@
-def call(String command) {
-    dir("${env.WORKSPACE}\\pipe@lib\\resources"){
-        sh "pwd"
-        bat String.format('nuget.exe %s', command)
-    }
-   // bat String.format('%s %s', pathToNuGet, command)
+def call(String pathToNuget, String command) {
+    bat String.format('%s %s', pathToNuget, command)
 }

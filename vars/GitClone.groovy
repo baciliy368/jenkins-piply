cbeff7007450fe4dbcd2ec8142b9e99bacbede47
d2.orlov <orlovfinder@gmail.com>
2020-05-05 11:26:10 +0300

    фыв

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -1,3 +1,3 @@
 def call(String credentials, String  branch, String url) {
-    git poll: false, changelog: false, url: url, credentialsId: credentials, branch: branch
+    git branch: branch, changelog: false, credentialsId: credentials, poll: false, url: url
 }

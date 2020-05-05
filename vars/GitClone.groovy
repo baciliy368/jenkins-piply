15d811645a75894daceec3e46d190e636c362018
d2.orlov <orlovfinder@gmail.com>
2020-05-05 11:41:52 +0300

    фыв

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -1,4 +1,4 @@
-def call(String credentials, String  branch, String url) {
-    git branch: branch, credentialsId: '6a6c48eb-48f3-4481-a049-e6c170f076aa', url: url
-    //git branch: branch, changelog: false, credentialsId: credentials, poll: false, url: url
+def call(String  branch, String credentials, String url) {
+    cleanWs()
+    git branch: branch, credentialsId: credentials, url: url
 }
